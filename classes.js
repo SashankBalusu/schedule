baf3000f069b1d7b764cc7763bfe1bc42454ba5b
Sashank Balusu <sashankbalusu@gmail.com>
2023-07-27 21:54:28 -0700

    mobile check

--- classes.js
+++ classes.js
@@ -32,6 +32,35 @@ function removeAllChildNodesTrue(parent) {
         parent.removeChild(parent.firstChild);
     }
 }
+
+const isUserUsingMobile = () => {
+
+    // User agent string method
+    let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    
+    // Screen resolution method
+    if (!isMobile) {
+        let screenWidth = window.screen.width;
+        let screenHeight = window.screen.height;
+        isMobile = (screenWidth < 768 || screenHeight < 768);
+    }
+    
+    // Touch events method
+    if (!isMobile) {
+        isMobile = (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+    }
+    
+    // CSS media queries method
+    if (!isMobile) {
+        let bodyElement = document.getElementsByTagName('body')[0];
+        isMobile = window.getComputedStyle(bodyElement).getPropertyValue('content').indexOf('mobile') !== -1;
+    }
+    
+    return isMobile
+    }
+if (isUserUsingMobile()) {
+    alert ("USE A COMPUTER FOR THIS SITE TRUST")
+}
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const name = urlParams.get('name')
